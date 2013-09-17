03d7aec258fbbd4482b5c111335cc93b21ca2621
guangyi <zhouguangyi2009@gmail.com>
2013-09-17 16:42:21 -0400

    project 4, interactive dots with mouseMove and mouseDown/Up animation event.

--- 4.Interactive-dots/Interactive-dots.js
+++ 4.Interactive-dots/Interactive-dots.js
@@ -5,26 +5,37 @@ var mouse = {x: 0, y: 0};
 var dotsArray = [];
 var distance;
 var mouseMove = false;
+var mouseDown = false;
+var mouseUp = false;
+
 function init(){
 	canvas = document.getElementById('myCanvas');
 	context = canvas.getContext("2d");
-	canvas.addEventListener("mousemove", mouseOver, false);
+	// These event are added to element canvas, not the certain content within canvas
+	canvas.addEventListener("mousemove", mousemove, false);
+	canvas.addEventListener("mousedown", mousedown, false);
+	canvas.addEventListener("mouseup", mouseup, false);
 	canvas.width = canvas.clientWidth;
 	canvas.height =  canvas.clientHeight;
-	//canvas.addEventListener("mouseout", mouseOut,false);
+	
 	var dot1 = new Dots();	
 	dotsArray.push(dot1);
 	dot1.draw();
-	setInterval(update, 1);
+	// For chrome, 17ms got the smoothest animation
+	// even though the dot stay still on the canvas, but still need setInterval()
+	// to run update function every 17ms because it needs to know when the mouse change
+	// its position and then fire the update function
+	setInterval(update, 17);
 }
-
-
 function Dots(){
-	this.radius = Math.floor(5 + Math.random() * 20);
+	//initX and initY and initRadius is to set the dot to it's original state.
+	//if it's too small, the  mousemove animation doesn't function very well
+	this.radius = Math.floor(10 + Math.random() * 20);
 	this.initRadius = this.radius;
 	this.x = this.radius  + Math.random() * (canvas.width -  2 * this.radius);
 	this.y = Math.random() * canvas.height;
-	
+	this.initX = this.x;
+	this.initY = this.y;
 	this.color = 'rgb(' + parseInt(Math.random() * 255) + ', ' +
 				parseInt(Math.random() * 255) + ', ' +
 				parseInt(Math.random() * 255) + ')';
@@ -38,13 +49,20 @@ Dots.prototype.draw = function(){
 	context.fill();
 }
 
-function mouseOver(event){
+function mousemove(event){
 	//event.clientX is the mouse position from viewport
 	//but dots on canvas has it's own position regarding canvas. 
 	//canvas.offsetLeft/Top to calculate the mouse position regarding canvas
 	mouse.x = event.clientX - canvas.offsetLeft;
 	mouse.y = event.clientY - canvas.offsetTop;
-	
+}
+function mousedown(event){
+	mouseDown = true;
+	mouseUp = false;
+}
+function mouseup(event){
+	mouseDown = false;
+	mouseUp = true;
 }
 
 function addDots(){
@@ -53,25 +71,57 @@ function addDots(){
 	dot.draw();
 }
 function update(){
-
+	context.clearRect(0, 0, canvas.width, canvas.height);
 	for (i = 0; i<dotsArray.length; i++){
-		distance = Math.sqrt(Math.pow(mouse.x - dotsArray[i].x, 2) + Math.pow(mouse.y - dotsArray[i].y, 2)); 
-		if (distance <= dotsArray[i].radius){
-			mouseMove = true;
-			if (dotsArray[i].radius < 40){
-				dotsArray[i].radius = dotsArray[i].radius + 1;
-				//dotsArray[i].draw();
-			}
-			
+		var dot = dotsArray[i]
+		// calculate the distance between the dot's center and current mouse position
+		distance = Math.sqrt(Math.pow(mouse.x - dot.x, 2) + Math.pow(mouse.y - dot.y, 2)); 
+		// if the mouse is pointing at the dot, the dot becomes larger.
+		if (distance <= dot.radius){
+			//mouseMove = true;
+			if(dot.radius < 45){
+				dot.radius = dot.radius + 3;
+			}			
 		}
-		if (mouseMove && distance > dotsArray[i].radius){
-			if(dotsArray[i].radius != dotsArray[i].initRadius){
-				dotsArray[i].radius = dotsArray[i].radius - 1;
-				context.clearRect(0, 0, canvas.width, canvas.height);
-								
+		// if the mouse point outside the dot
+		// the dot will get smaller until its original radius
+		if(distance > dot.radius){
+			if(dot.radius != dot.initRadius){
+				dot.radius = dot.radius - 1;				
 			}
 		}
-		dotsArray[i].draw();
+		// if click any where on the canvas, without mouseup
+		// all the dots that 50 pixel away will come toward where the mouse points at
+		if(mouseDown){
+			// xDirection and yDirection is to mark which direction to go
+			// right or left, top or down
+			var xDirection;
+			var yDirection;
+			if (distance > 50){
+				if (mouse.x > dot.x){
+					xDirection = 1;
+				}
+				else {
+					xDirection = -1;
+
+				}
+				if (mouse.y > dot.y){
+					yDirection = 1;
+				}
+				else {
+					yDirection = -1;
+
+				}
+				dot.x = dot.x + xDirection * 2;
+				dot.y = dot.y + yDirection * 2;
+			}
+		}
+		// when mouse up, the dot go back to its original position
+		if(mouseUp){
+			dot.x = dot.initX;
+			dot.y = dot.initY;
+		}
+		dot.draw();
 	}
 }
 window.onload = init;
