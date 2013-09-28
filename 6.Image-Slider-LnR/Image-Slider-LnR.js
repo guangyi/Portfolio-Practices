e4daa58eb8790af4709adb4e9801d0dd2b7eb376
guangyi <zhouguangyi2009@gmail.com>
2013-09-28 18:18:42 -0400

    redesign the code, has better architecture.with the help of Zhisheng.

--- 6.Image-Slider-LnR/Image-Slider-LnR.js
+++ 6.Image-Slider-LnR/Image-Slider-LnR.js
@@ -1,52 +1,31 @@
 
 //1. set ul width 
-//2. image-slider when click prev/next button
+//2. image when click prev/next button
 var ul;
 var li_items;
-var image_number;
-var image_width;
+var imageNumber;
+var imageWidth;
 var prev, next;
-var current = 0;
+var currentPostion = 0;
+var currentImage = 0;
+
 
 function init(){
 	ul = document.getElementById('image_slider');
 	li_items = ul.children;
-	image_number = li_items.length;
-	image_width = li_items[0].children[0].clientWidth;
-	ul.style.width = parseInt(image_width * image_number) + 'px';
+	imageNumber = li_items.length;
+	imageWidth = li_items[0].children[0].clientWidth;
+	ul.style.width = parseInt(imageWidth * imageNumber) + 'px';
 	prev = document.getElementById("prev");
 	next = document.getElementById("next");
 	//.onclike = slide(-1) will be fired when onload;
+	/*
 	prev.onclick = function(){slide(-1);};
-	next.onclick = function(){slide(1);};
+	next.onclick = function(){slide(1);};*/
+	prev.onclick = function(){ onClickPrev();};
+	next.onclick = function(){ onClickNext();};
 }
 
-function slide(direction){
-	var currentLeft = -1 * current * image_width;
-	var opts1 = {
-		duration:1000,
-		delta:function(p){return p;},
-		step:function(delta){
-			ul.style.left = parseInt(currentLeft - direction * delta * image_width) + 'px';
-		},	
-	};
-	var opts2 = {
-		duration:2000,
-		delta:function(p){return p;},
-		step:function(delta){
-			ul.style.left = parseInt(currentLeft + direction * ((image_number - 1) * image_width * delta)) + 'px';
-		},
-	};
-			
-	if ((direction == -1 && current == 0) || (direction == 1 && current == image_number-1)){
-		id = animate(opts2);	
-		current = image_number - 1 - current;	
-	}
-	else{
-		current = current + direction;
-		id = animate(opts1);
-	}	
-}
 function animate(opts){
 	var start = new Date;
 	var id = setInterval(function(){
@@ -59,9 +38,46 @@ function animate(opts){
 		opts.step(delta);
 		if (progress == 1){
 			clearInterval(id);
+			opts.callback();
 		}
 	}, opts.delay || 17);
-	return id;
-
+	//return id;
 }
+
+function slideTo(imageToGo){
+	var direction;
+	var numOfImageToGo = Math.abs(imageToGo - currentImage);
+	// slide toward left
+
+	direction = currentImage > imageToGo ? 1 : -1;
+	currentPostion = -1 * currentImage * imageWidth;
+	var opts = {
+		duration:1000,
+		delta:function(p){return p;},
+		step:function(delta){
+			ul.style.left = parseInt(currentPostion + direction * delta * imageWidth * numOfImageToGo) + 'px';
+		},
+		callback:function(){currentImage = imageToGo;}	
+	};
+	animate(opts);
+}
+
+function onClickPrev(){
+	if (currentImage == 0){
+		slideTo(imageNumber - 1);
+	} 		
+	else{
+		slideTo(currentImage - 1);
+	}		
+}
+
+function onClickNext(){
+	if (currentImage == imageNumber - 1){
+		slideTo(0);
+	}		
+	else{
+		slideTo(currentImage + 1);
+	}		
+}
+
 window.onload = init;
