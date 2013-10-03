4735bae4b67453bc6becbb2ea6be48192c5a2839
guangyi <zhouguangyi2009@gmail.com>
2013-10-03 16:34:35 -0400

    added comments and delete those useless codes

--- 6.Image-Slider-LnR/Image-Slider-LnR.js
+++ 6.Image-Slider-LnR/Image-Slider-LnR.js
@@ -1,8 +1,7 @@
-
 //1. set ul width 
 //2. image when click prev/next button
 var ul;
-var li_items;
+var liItems;
 var imageNumber;
 var imageWidth;
 var prev, next;
@@ -12,20 +11,24 @@ var currentImage = 0;
 
 function init(){
 	ul = document.getElementById('image_slider');
-	li_items = ul.children;
-	imageNumber = li_items.length;
-	imageWidth = li_items[0].children[0].clientWidth;
+	liItems = ul.children;
+	imageNumber = liItems.length;
+	imageWidth = liItems[0].children[0].clientWidth;
+	// set ul's width as the total width of all images in image slider.
 	ul.style.width = parseInt(imageWidth * imageNumber) + 'px';
 	prev = document.getElementById("prev");
 	next = document.getElementById("next");
-	//.onclike = slide(-1) will be fired when onload;
-	/*
+	/*.onclike = slide(-1) will be fired when onload;
 	prev.onclick = function(){slide(-1);};
-	next.onclick = function(){slide(1);};*/
+	next.onclick = function(){slide(1);};
+	event bubbling */
 	prev.onclick = function(){ onClickPrev();};
 	next.onclick = function(){ onClickNext();};
 }
-
+/**
+* animate is a generic function for animation. 
+* but what the animation does, doesn't defined here
+*/
 function animate(opts){
 	var start = new Date;
 	var id = setInterval(function(){
@@ -41,14 +44,18 @@ function animate(opts){
 			opts.callback();
 		}
 	}, opts.delay || 17);
-	//return id;
 }
-
+/**
+* slideTo is the function that actually does the movement.
+* it takes one variable--imageToGo as parameter. it's an int stands for the image will be displayed
+* By comparing imageToGo and currentImage, it can be decided which direction to move, left or right
+* left: direction = -1; right: direction = 1
+* so the new left position is the current postion plus/minus (number of imagesToGo * image width)
+* when the step function is finished, a callback function will be called to set current image to imageToGo
+**/
 function slideTo(imageToGo){
 	var direction;
 	var numOfImageToGo = Math.abs(imageToGo - currentImage);
-	// slide toward left
-
 	direction = currentImage > imageToGo ? 1 : -1;
 	currentPostion = -1 * currentImage * imageWidth;
 	var opts = {
@@ -62,6 +69,10 @@ function slideTo(imageToGo){
 	animate(opts);
 }
 
+/**
+* clicking prev. if current image is the first image, ul slide all the way to the last one
+* otherwise, it slide to the image on the left of current image.
+**/
 function onClickPrev(){
 	if (currentImage == 0){
 		slideTo(imageNumber - 1);
@@ -70,7 +81,10 @@ function onClickPrev(){
 		slideTo(currentImage - 1);
 	}		
 }
-
+/**
+* clicking next. if current image is the last image, ul slide all the way to the first one
+* otherwise, it slide to the image on the right of current image.
+**/
 function onClickNext(){
 	if (currentImage == imageNumber - 1){
 		slideTo(0);
--- 7.Image-Slider-Pager/Image-Slider-Pager.js
+++ 7.Image-Slider-Pager/Image-Slider-Pager.js
@@ -42,14 +42,18 @@ function animate(opts){
 			opts.callback();
 		}
 	}, opts.delay || 17);
-	//return id;
 }
-
+/**
+* slideTo is the function that actually does the movement.
+* it takes one variable--imageToGo as parameter. it's an int stands for the image will be displayed
+* By comparing imageToGo and currentImage, it can be decided which direction to move, left or right
+* left: direction = -1; right: direction = 1
+* so the new left position is the current postion plus/minus (number of imagesToGo * image width)
+* when the step function is finished, a callback function will be called to set current image to imageToGo
+**/
 function slideTo(imageToGo){
 	var direction;
 	var numOfImageToGo = Math.abs(imageToGo - currentImage);
-	// slide toward left
-
 	direction = currentImage > imageToGo ? 1 : -1;
 	currentPostion = -1 * currentImage * imageWidth;
 	var opts = {
@@ -63,6 +67,10 @@ function slideTo(imageToGo){
 	animate(opts);
 }
 
+/**
+* clicking prev. if current image is the first image, ul slide all the way to the last one
+* otherwise, it slide to the image on the left of current image.
+**/
 function onClickPrev(){
 	if (currentImage == 0){
 		slideTo(imageNumber - 1);
@@ -71,7 +79,10 @@ function onClickPrev(){
 		slideTo(currentImage - 1);
 	}		
 }
-
+/**
+* clicking next. if current image is the last image, ul slide all the way to the first one
+* otherwise, it slide to the image on the right of current image.
+**/
 function onClickNext(){
 	if (currentImage == imageNumber - 1){
 		slideTo(0);
@@ -80,7 +91,9 @@ function onClickNext(){
 		slideTo(currentImage + 1);
 	}		
 }
-
+/**
+*generatePager automatically. no need to change when image number changes.	
+*/
 function generatePager(imageNumber){	
 	var pageNumber;
 	var pagerDiv = document.getElementById('pager');
@@ -95,12 +108,14 @@ function generatePager(imageNumber){
 				slideTo(i);
 			}
 		}(i);
-	}	
-	//ul.children.onclick = (function(){slideTo(this.)}
+	}
+	// style.width can't get width from css sheet. 	
 	var computedStyle = document.defaultView.getComputedStyle(li, null);
 	//border width 1px; offsetWidth = 22
 	var liWidth = parseInt(li.offsetWidth);
+	// remove px from the string returned. like '5px'-->'5'
 	var liMargin = parseInt(computedStyle.margin.replace('px',""));
+	// margin on both left and right side.
 	pagerDiv.style.width = parseInt((liWidth + liMargin * 2) * imageNumber) + 'px';
 }
 window.onload = init;
